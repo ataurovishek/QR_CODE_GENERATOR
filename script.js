7017053b04c70fdaa1b3089dc1cfe9c1e4780adb
ovi <ataurovishek@gmail.com>
2024-01-07 00:21:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const button = document.getElementById('Generate-btn')
+
+const generateCode = (e) => {
+
+    const url = document.getElementById('link-input').value;
+    const Qr_Code = document.getElementById('Qr_Code');
+
+
+    Qr_Code.innerHTML = ''
+    
+    var New_QrCode = new QRCode('Qr_Code', {
+        text: url,
+        width: 128,
+        height: 128,
+        colorDark: '#000000'
+    })   
+}
+
+button.addEventListener('click', generateCode)
